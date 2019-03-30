32a79556ba16df0b925db716f4217c3f413b2d79
g k <gkosmo1@hotmail.com>
2019-03-30 13:05:36 +0100

    added

---
+++ app/views/problems/_problem.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! problem, :id, :status, :category_id, :title, :description, :user_id, :status, :created_at, :updated_at
+json.url problem_url(problem, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :comments
+  resources :problems
+  resources :categories
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
