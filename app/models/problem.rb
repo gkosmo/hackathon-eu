3c08ba73d18de4188dc714729b5adbfabe6cba44
Guillaume Rossi <guillaum.ro@gmail.com>
2019-03-30 22:03:25 +0100

    searching through tags, title, description, categories

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -6,19 +6,29 @@ class Problem < ApplicationRecord
 
   acts_as_votable
 
+  include PgSearch
+
+  pg_search_scope :global_search,
+                  against: %i[title description],
+                  associated_against: {
+                    category: [:title],
+                    comments: [:description],
+                    tags: [:name]
+                  },
+                  using: {
+                    tsearch: { prefix: true } # <-- now `superman batm` will return something!
+                  }
 
   def self.by_countries
-
-    url = "https://restcountries.eu/rest/v2/regionalbloc/eu" 
-    page_json =  HTTParty.get(url)
-     countries = JSON.parse(page_json.body)
-     countries_name_list = { }
-     countries.each do  |country| 
-         transl = country['translations'].map{|k, v|  v.downcase } 
-         p transl
-         countries_name_list[country['name']]  =  Problem.tagged_with( [ country['name']&.downcase, country['altSpellings'].map(&:downcase), transl ].flatten, :any => true ).count 
-     end
+    url = "https://restcountries.eu/rest/v2/regionalbloc/eu"
+    page_json = HTTParty.get(url)
+    countries = JSON.parse(page_json.body)
+    countries_name_list = {}
+    countries.each do |country|
+      transl = country['translations'].map { |_k, v| v.downcase }
+      p transl
+      countries_name_list[country['name']] = Problem.tagged_with([country['name']&.downcase, country['altSpellings'].map(&:downcase), transl].flatten, any: true).count
+    end
     countries_name_list
-
   end
 end
