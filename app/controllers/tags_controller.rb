d729c1da89d8c7a543a501a2092fe63e7fda1eba
Guillaume Rossi <guillaum.ro@gmail.com>
2019-03-30 20:16:45 +0100

    destroy tags and add tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -8,9 +8,16 @@ class TagsController < ApplicationController
   end
 
   def destroy
-    @tag =  ActsAsTaggableOn::Tag.find(params[:id])
-    @tag.destroy
-    redirect_to root
+    @problem = Problem.find(params[:id])
+    @problem.tag_list.remove(params[:problem_id], parse: true)
+    respond_to do |format|
+      if @problem.save
+        format.html { redirect_to @problem, notice: 'Problem was successfully updated.' }
+        format.json { render :show, status: :ok, location: @problem }
+      else
+        format.html { render :edit }
+        format.json { render json: @problem.errors, status: :unprocessable_entity }
+      end
+    end
   end
-
 end
