da9bad2744ee8404e32448bcd3a6594b198058e0
g k <gkosmo1@hotmail.com>
2019-03-31 07:29:26 +0200

    added dashboard

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ application.load(definitionsFromContext(context))
 
 
 let dashboard = document.querySelector('.dashboard-container')
+
 if(dashboard){ 
 
     const program_tab = document.querySelector('#tab-created')
@@ -19,6 +20,8 @@ if(dashboard){
     const poetry_div = document.querySelector('.followed')
     const array_tabs = [ program_tab, visual_tab, poetry_tab ]
     const array_divs = [ program_div, visual_div, poetry_div]
+    console.log(array_tabs)
+    console.log(array_divs)
     function cleanAll(){
     array_tabs.forEach((tab) => {
         tab.classList.remove('underlines')
