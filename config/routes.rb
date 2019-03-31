435aaf922a0c0b1065499fb42af5234db4fe9112
Boris Josz <Boris.Josz@gmail.com>
2019-03-31 08:09:20 +0200

    homepage created, made as application root

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
   end
   resources :categories
   devise_for :users
-  root to: 'problems#index'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    get 'analytics', to: "pages#analytics"
    get 'dashboard', to: "pages#dashboard"
