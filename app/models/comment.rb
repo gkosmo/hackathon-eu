4f9dab9273845bff0c8b20ac8d3b81d07da2de4f
g k <gkosmo1@hotmail.com>
2019-03-31 08:47:17 +0200

    push sur master

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :problem
-  belongs_to :user
+  belongs_to :user, optional: true
   acts_as_votable
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -29,10 +29,10 @@ Category.create!(title: "Funding a business")
 Category.create!(title: "Public contracts")
 Category.create!(title: "Health and safety at work")
 users =[ User.create(email: 'a@a.com', password: "123123") ] 
-users << User.create(email: 'a@a.com', password: "123123")
-users << User.create(email: 'a@a.com', password: "123123")
-users << User.create(email: 'a@a.com', password: "123123")
-users << User.create(email: 'a@a.com', password: "123123")
+users << User.create(email: 'b@a.com', password: "123123")
+users << User.create(email: 'c@a.com', password: "123123")
+users << User.create(email: 'd@a.com', password: "123123")
+users << User.create(email: 'e@a.com', password: "123123")
 
 p problem_1 = Problem.create!(title: "Can I use my driving licence as an ID document at the border?", description: "I'm Portuguese and am going to Cyprus this summer for holidays. I'm taking the ferry with my car on a cruise and they accepted my driving licence. Is that enough ? ", category: travel)
 p problem_1.comments.create!(description: "Is it with the MedTour Cruise ? I'm French and I have the same problem", user: users.sample )
@@ -112,10 +112,10 @@ page_json =  HTTParty.get(url)
     problems.each do |problem| 
         
         problem.tag_list.add(tag)
-        problem.save
+        problem.save!
     end
  end
 
  Comment.all.each do |comment|
-  p  comment.update created_at: Faker::Time.backward(200, :evening)
+  p  comment.update! created_at: Faker::Time.backward(200, :evening)
 end
