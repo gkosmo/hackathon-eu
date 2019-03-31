1afffe51afcd5f54006b1b5906f66c6dd72995db
g k <gkosmo1@hotmail.com>
2019-03-31 08:41:42 +0200

    adde d

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,16 @@
 module ApplicationHelper
 
     def user_upvote_color(user, votable) 
-        user.voted_up_on?(votable) ?  'ThumbsUp_icon_blue.png' : 'ThumbsUp_icon.png'
+        user&.voted_up_on?(votable) ?  'ThumbsUp_icon_blue.png' : 'ThumbsUp_icon.png'
     end 
 
 
     def user_downvote_color(user, votable) 
-        user.voted_down_on?(votable) ? 'ThumbsDown_icon_blue.png' : 'ThumbsDown_icon.png' 
+        user&.voted_down_on?(votable) ? 'ThumbsDown_icon_blue.png' : 'ThumbsDown_icon.png' 
     end 
 
     def follow_problem(user, problem)
         problems = user&.follows
         problems&.select { |v| v.problem == problem}&.first
-        
     end
 end
